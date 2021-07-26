fa23822fee64018c55f0e8ae5415a58fce5943f8
MH.Ji <battlesun99@gmail.com>
2021-07-26 12:22:44 +0900

    Feat: filling canvas with click event

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const canvas = document.getElementById("jsCanvas")
 const ctx = canvas.getContext("2d") // context란 canvas내의 픽셀에 접근할 수 있는 방법
 const colors = document.getElementsByClassName("jsColor")
+const range = document.getElementById("jsRange")
+const mode = document.getElementById("jsMode")
 
-canvas.width = 700
-canvas.height = 700
-ctx.strokeStyle = "#2C2C2C"
+const DEFAULT_COLOR = "#2C2C2C"
+const DEFAULT_SIZE = 700
+
+canvas.width = DEFAULT_SIZE
+canvas.height = DEFAULT_SIZE
+ctx.strokeStyle = DEFAULT_COLOR
+ctx.fillStyle = DEFAULT_COLOR
 ctx.lineWidth = 2.5
 
 let painting = false
+let filling = false
+let x
+let y
 
 function startPainting() {
     painting = true
@@ -22,8 +31,8 @@ function onMouseDown(event) {
 }
 
 function onMouseMove(event) {
-    const x = event.offsetX
-    const y = event.offsetY
+    x = event.offsetX
+    y = event.offsetY
 
     if (painting === false) { // 마우스를 손에서 떼어냈을 경우
         ctx.beginPath() // 선을 만듦
@@ -39,11 +48,44 @@ if (canvas) {
     canvas.addEventListener("mousedown", startPainting)
     canvas.addEventListener("mouseup", stopPainting)
     canvas.addEventListener("mouseleave", stopPainting)
+    canvas.addEventListener("click", onClickCanvas)
 }
 
 function onSelectColor(event) {
     const color = event.target.style.backgroundColor
     ctx.strokeStyle = color
+    ctx.fillStyle = color
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", onSelectColor))
+
+function onChangeRange(event) {
+    const size = event.target.value
+    ctx.lineWidth = size
+}
+
+if (range) {
+    range.addEventListener("input", onChangeRange)
+}
+
+// canvas 전체를 색으로 채워주는 function
+function onClickMode(event) {
+    if (filling === true) {
+        filling = false
+        mode.innerText = "Fill"
+    } else {
+        filling = true
+        mode.innerText = "Paint"
+    }
+}
+
+if (mode) {
+    mode.addEventListener("click", onClickMode)
+}
+
+function onClickCanvas() {
+    if (filling === true) {
+        ctx.rect(0, 0, DEFAULT_SIZE, DEFAULT_SIZE)
+        ctx.fill()
+    }
+}
